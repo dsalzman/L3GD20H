20ab90cf21d56e53e81ea045a399bb0e0ab084bc
Danny Salzman <dsalzman@utexas.edu>
2015-06-17 09:22:18 -0700

    Added Arduino Code

---
+++ Arduino_Pro_Mini/Arduino_Pro_Mini.ino
@@ -0,0 +1,32 @@
+// include the lirary code
+#include <SoftwareSerial.h>
+#include <LiquidCrystal.h>
+/**********************************************************/
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(4, 6, 10, 11, 12, 13);
+SoftwareSerial mySerial(0, 1); // RX, TX
+/*********************************************************/
+void setup()
+{
+  lcd.begin(16, 2);  // set up the LCD's number of columns and rows: 
+  Serial.begin(9600);
+  mySerial.begin(9600);
+}
+/*********************************************************/
+void loop() 
+{
+   if(mySerial.available()){
+      char c = mySerial.read();
+      if(c == 'q'){
+        lcd.clear();
+      }
+      else if(c == '!'){
+        lcd.setCursor(0,0);   
+      }
+      else if(c == '@'){
+        lcd.setCursor(0,1);
+      }
+      else lcd.print(c);
+   }
+}
+/************************************************************/
